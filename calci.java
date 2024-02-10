32bbdb9aeb693bd92e5cf43e0e975b5b599d83d9
venkateshkanadam <venkykanadam9663@gmail.com>
2024-02-10 10:19:56 +0530

    first commit

---
+++ calci.java
@@ -0,0 +1,13 @@
+class calci
+{
+	public static void main(String args[])
+	{
+		 int a = 10;
+		 int b = 20;
+		 int c = a-b;
+		 int d = a*b;
+		 int e = a/b;
+		 int f = a%b;
+		 System.out.println("The a and b sum is:"+c);
+	}
+}
