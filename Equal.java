319308a6ce1449e0fcf5cef0b7c9e62bcfac0cd9
venkateshkanadam <venkykanadam9663@gmail.com>
2024-02-10 10:33:17 +0530

    Third commit

---
+++ Equal.java
@@ -0,0 +1,17 @@
+class Equal
+{
+	public static void main(String args[])
+	{
+		 int a = 17;
+		 int b = 17;
+		 if(a==b)
+		 {
+		 System.out.println("same");
+		 }
+		 else
+		 {
+			System.out.println("not same");
+		 }
+	 }
+}
+
