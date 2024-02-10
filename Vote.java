f1706bf04034b8e640935a167652f58458838cea
venkateshkanadam <venkykanadam9663@gmail.com>
2024-02-10 10:24:36 +0530

    second commit

---
+++ Vote.java
@@ -0,0 +1,16 @@
+class Vote
+{
+	public static void main(String args[])
+	{
+		 int a = 17;
+		 if(a>19)
+		 {
+			System.out.println("Eligible for vote");
+		 }
+		 else
+		 {
+			System.out.println("not eligible");
+		 }
+	 }
+}
+
